cb99675b4f78b42b0a82af960944644d13ac7d3b
kboueb <kboueb@gmail.com>
2022-04-28 21:05:23 +0000

    1 day

---
+++ jour1-band-name-generator.py
@@ -0,0 +1,5 @@
+print("Bienvenu sur le band name generator")
+city = input("Quelle est le nom de votre ville préférée? \n")
+pet = input("Quel nom donnerez-vous à votre animal de compagnie? \n")
+
+print(f"Votre band name est {pet}{city} \n")
